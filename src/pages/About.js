a9371a13974e25c575a3afff3e2a4b91c9676a52
Ukhsenbaas <ukhsenbaas@LEAP-10s-iMac.local>
2022-05-09 11:30:13 +0800

    5dahi ungaalt

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,5 @@
-// import { db } from "../firebase";
-import { useState } from "react";
-import { doc, onSnapshot } from "firebase/firestore";
+
 
 export const About = () => {
-  //   window.addEventListener("load", () => {
-  //     Fetchdata();
-  //   });
-
-  //   const Fetchdata = () => {
-  //     db.collection("data")
-  //       .get()
-  //       .then((querySnapshot) => {
-  //         querySnapshot.forEach((element) => {
-  //           let data = element.data();
-  //           setInfo((arr) => [...arr, data]);
-  //         });
-  //       });
-  //   };
-
   return <h1>info</h1>;
 };
